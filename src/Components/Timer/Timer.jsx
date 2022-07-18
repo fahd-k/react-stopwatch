5c3df3c1942ea4dad77c4066013c24b0af1cc9f7
Fahd Ksara <fahd.ksara@weareadaptive.com>
2022-07-18 11:52:23 -0400

    changes to timer and reducer file

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -7,7 +7,7 @@ export default function Timer(timerCount) {
     return (
     <body>
       <div class="timer-wrapper">
-        <h1 class="timer">{timerCount ? formattedTime(timerCount) : '00:00.00'}</h1>
+        <h1 class="timer">{formattedTime(timerCount)}</h1>
       </div>
       </body>
     );
